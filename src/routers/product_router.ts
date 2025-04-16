966a358e6afc67d74c8623e92221d772ea16fd5c
Gustavo Müller <gustavomuller@MacBook-Air-de-Gustavo.local>
2025-04-16 19:31:11 -0300

    config repo

--- src/routers/product_router.ts
+++ src/routers/product_router.ts
@@ -5,22 +5,24 @@ import multer from "multer";
 import product_controller from '../controller/product_controller';
 
 const router = express.Router();
-const upload = multer({dest: '/uploads/'})
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // prefixo: /api/produtos
 
-//listar produtos
-router.get('/', product_controller.list)
+// listar produtos
+router.get('/', product_controller.list);
 
-//inserir produto
-router.post('/', product_controller.insert)
+// inserir produto
+router.post('/', product_controller.insert);
 
-//buscar produto por id
-router.get('/:id', product_controller.searchById)
+// buscar produto por id
+router.get('/:id', product_controller.searchById);
 
 router.post('/images/upload', upload.single('image'), async (req: Request, res: Response): Promise<any> => {
     console.log(req.file);
     res.send("Imagem carregada com sucesso");
-})
+});
 
 export default router;
